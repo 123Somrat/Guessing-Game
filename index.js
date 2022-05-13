77fe20ecb05db95db9b83e566e0c6e90f8a6bed6
Mohammad Jafar Uddin <mdjafaruddinsomrat@gmail.com>
2022-05-13 12:10:49 +0200

    finding UI Element

---
+++ index.js
@@ -0,0 +1,9 @@
+// finding UI Element
+
+const form = document.querySelector("form");
+const cardBody = document.querySelector(".card-body");
+const guessingNumber = form.querySelector("#guessingNumber");
+const checkButton = form.querySelector("#check");
+const resultText = cardBody.querySelector("#resultText");
+const remainingAttempts = cardBody.querySelector(".remainingAttempts")
+
